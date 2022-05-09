baf1f16810695fe4d72f7247211036c8de4ab729
Yuri <yuri.ferreira@flexpeak.com.br>
2022-05-09 12:02:18 -0400

    adicionando site

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<title>Encontro Sinodal</title>
+	<meta content="" name="description">
+	<meta content="" name="keywords">
+
+	<!-- Favicons -->
+	<link href="/site/assets/img/favicon.png" rel="icon">
+	<link href="/site/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<!-- Vendor CSS Files -->
+	<link href="/site/assets/vendor/aos/aos.css" rel="stylesheet">
+	<link href="/site/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/site/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="/site/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="/site/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	<link href="/site/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+	<link href="/site/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+	<link href="/site/assets/css/style.css" rel="stylesheet">
+
+	<!-- =======================================================
+	* Template Name: Arsha - v4.7.1
+	* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+	* Author: BootstrapMade.com
+	* License: https://bootstrapmade.com/license/
+	======================================================== -->
+</head>
+
+<body>
+
+	<!-- ======= Header ======= -->
+	<header id="header" class="fixed-top ">
+		<div class="container d-flex align-items-center">
+
+			<h1 class="logo me-auto"><a href="index.html">Encontro Sinodal</a></h1>
+			<!-- Uncomment below if you prefer to use an image logo -->
+			<!-- <a href="index.html" class="logo me-auto"><img src="/site/assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+			<nav id="navbar" class="navbar">
+				<ul>
+					<li><a class="nav-link scrollto active" href="#about">Informações</a></li>
+					<li><a class="getstarted scrollto" href="#contact">Inscrever-se</a></li>
+				</ul>
+				<i class="bi bi-list mobile-nav-toggle"></i>
+			</nav><!-- .navbar -->
+
+		</div>
+	</header><!-- End Header -->
+
+	<!-- ======= Hero Section ======= -->
+	<section id="hero" class="d-flex align-items-center">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+					<h1>FÉ E A CIDADANIA DESTE MUNDO</h1>
+					<h2>Rev. Heber Campos Jr</h2>
+				</div>
+				<div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+					<img src="/site/assets/img/encontro.png" class="img-fluid animated" alt="">
+				</div>
+			</div>
+		</div>
+
+	</section><!-- End Hero -->
+
+	<main id="main">
+
+		<!-- ======= Clients Section ======= -->
+		<section id="clients" class="clients section-bg">
+			<div class="container">
+
+				<div class="row" data-aos="zoom-in">
+
+					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+						<h3></h3>
+					</div>
+					
+					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+						<h3>FMS</h3>
+					</div>
+
+					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+						<h3>FEPAM</h3>
+					</div>
+
+					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+						<h3>FMRR</h3>
+					</div>
+
+					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+						<h3>FPSAM</h3>
+					</div>
+
+
+					<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+						<h3></h3>
+					</div>
+
+				</div>
+
+			</div>
+		</section><!-- End Cliens Section -->
+
+		<!-- ======= About Us Section ======= -->
+		<section id="about" class="about">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>Sobre o Evento</h2>
+				</div>
+
+				<div class="row content">
+					<div class="col-lg-6">
+						<p>
+							TEXTO
+						</p>
+						<ul>
+							<li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+							<li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
+							<li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+						</ul>
+					</div>
+					<div class="col-lg-6 pt-4 pt-lg-0">
+						<p>
+							Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+							velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+							culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+						<div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+							<div class="pic"><img src="/site/assets/img/heber.png" class="img-fluid" alt=""></div>
+							<div class="member-info">
+								<h4>Rev. Heber Carlos de Campos Jr </h4>
+								<span>Palestrante</span>
+								<p>É ministro presbiteriano desde junho de 1999. Doutor em Teologia Histórica pelo Calvin Theological Seminary, EUA (2009).</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+
+			</div>
+		</section><!-- End About Us Section --
+			<!-- ======= Services Section ======= -->
+		<section id="services" class="services section-bg">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>Informações</h2>
+					<p>Gerais</p>
+				</div>
+
+				<div class="row">
+					<div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+						<div class="icon-box w-100">
+							<div class="icon text-center"><i class="bi bi-calendar2-event-fill"></i></div>
+							<h4 class="text-center"><a href="">Data</a></h4>
+							<h4>21 a 24 de Outubro</h4>
+						</div>
+					</div>
+
+					<div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+						<div class="icon-box w-100">
+							<div class="icon text-center"><i class="bi bi-geo-alt-fill"></i></div>
+							<h4 class="text-center"><a href="">Local</a></h4>
+							<h4>Boa Vista - RR <br> Acampamento Maná</h4>
+						</div>
+					</div>
+
+					<div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+						<div class="icon-box w-100">
+							<div class="icon text-center"><i class="bi bi-currency-dollar"></i></div>
+							<h4 class="text-center"><a href="">Valor AM</a></h4>
+							<p>
+								<b>09/05/22 - 14/08/22</b>
+								<br>
+								R$130,00 (Ônibus não incluso)
+								<br>
+								<br>
+								<b>09/05/22 - 14/08/22</b>
+								<br>
+								R$130,00 (Ônibus não incluso)
+							</p>
+						</div>
+					</div>
+
+					<div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+						<div class="icon-box w-100">
+							<div class="icon text-center"><i class="bi bi-currency-dollar"></i></div>
+							<h4 class="text-center"><a href="">Valor RR</a></h4>
+							<p>
+								<b>09/05/22 - 14/08/22</b>
+								<br>
+								R$140,00
+								<br>
+								<br>
+								<b>09/05/22 - 14/08/22</b>
+								<br>
+								R$160,00 
+							</p>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</section><!-- End Services Section -->
+		<!-- ======= Vagas para o Onibus ======= -->
+		<section id="skills" class="skills">
+			<div class="container" data-aos="fade-up">
+
+				<div class="row">
+					<div class="col-lg-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="100">
+						<img src="/site/assets/img/viagem.svg" class="img-fluid" alt="">
+					</div>
+					<div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
+						<h3>Ônibus Manaus - Boa Vista</h3>
+						<div class="skills-content">
+							<div class="progress">
+								<span class="skill">Vagas Preenchidas <i class="val">100%</i></span>
+								<div class="progress-bar-wrap">
+									<div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+							</div>
+						</div>
+						<p>
+							<b>Para os encontristas do Amazonas</b>: Será alugado um ônibus que sairá de Manaus na noite do dia 21/out (Sexta-feira) e chegará em Boa Vista na manhã do dia 22/out (Sábado).
+							Voltando dia 24/out (Segunda-feira).
+						</p>
+						<p class="fst-italic">
+							Inicialmente estamos planejando apenas 1 ônibus. Caso a demanda seja maior, podemos planejar outro ônibus.
+							<br>
+						</p>
+						<p>
+							Quantidade de vagas: 50 <br>
+							Quantidade restantes: X <br>
+							Valor: R$170,00.
+						</p>
+					</div>
+				</div>
+
+			</div>
+		</section>
+		<!-- End ONIBUS -->
+
+
+		<!-- ======= PERGUNTAS FEREQUENTES ======= -->
+		<!-- <section id="faq" class="faq section-bg">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>Perguntas Frequentes</h2>
+					<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+				</div>
+
+				<div class="faq-list">
+					<ul>
+						<li data-aos="fade-up" data-aos-delay="100">
+							<i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1">Non consectetur a erat nam at lectus urna duis? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+							<div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+								<p>
+									Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+								</p>
+							</div>
+						</li>
+
+						<li data-aos="fade-up" data-aos-delay="200">
+							<i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-2" class="collapsed">Feugiat scelerisque varius morbi enim nunc? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+							<div id="faq-list-2" class="collapse" data-bs-parent=".faq-list">
+								<p>
+									Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+								</p>
+							</div>
+						</li>
+
+						<li data-aos="fade-up" data-aos-delay="300">
+							<i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-3" class="collapsed">Dolor sit amet consectetur adipiscing elit? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+							<div id="faq-list-3" class="collapse" data-bs-parent=".faq-list">
+								<p>
+									Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+								</p>
+							</div>
+						</li>
+
+						<li data-aos="fade-up" data-aos-delay="400">
+							<i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-4" class="collapsed">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+							<div id="faq-list-4" class="collapse" data-bs-parent=".faq-list">
+								<p>
+									Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in.
+								</p>
+							</div>
+						</li>
+
+						<li data-aos="fade-up" data-aos-delay="500">
+							<i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" data-bs-target="#faq-list-5" class="collapsed">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+							<div id="faq-list-5" class="collapse" data-bs-parent=".faq-list">
+								<p>
+									Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque.
+								</p>
+							</div>
+						</li>
+
+					</ul>
+				</div>
+
+			</div>
+		</section> -->
+		<!-- End Frequently Asked Questions Section -->
+
+		<!-- ======= Contact Section ======= -->
+		<section id="contact" class="contact">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>Inscrição</h2>
+					<p>Faça sua inscrição agora</p>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-12 mt-5 mt-lg-0 d-flex align-items-stretch">
+						<form action="{{route('inscricao.store')}}" method="post" role="form" class="php-email-form">
+							<div class="row">
+								<div class="form-group col-md-6">
+									<label for="nome">Nome</label>
+									<input type="text" name="nome" class="form-control" id="nome" required>
+								</div>
+								<div class="form-group col-md-6">
+									<label for="email">Email</label>
+									<input type="email" class="form-control" name="email" id="email" required>
+								</div>
+							</div>
+							<div class="row">
+								<div class="form-group col-md-6">
+									<label for="celular">Celular</label>
+									<input type="text" class="form-control" name="celular" id="celular" required>
+								</div>
+								<div class="form-group col-md-6">
+									<label for="federacao">Federação</label>
+									<select class="form-control" name="federacao" id="federacao">
+										<option value="FAMP">FAMP</option>
+										<option value="FEPAM">FEPAM</option>
+										<option value="FMS">FMS</option>
+										<option value="FMRR">FMRR</option>
+										<option value="FPSAM">FPSAM</option>
+										<option value="FPMAN">FPMAN</option>
+									</select>
+								</div>
+							</div>
+							<div class="row">
+								<div class="form-group col-md-6">
+									<label for="igreja">Igreja</label>
+									<input type="text" class="form-control" name="igreja" id="igreja" required>
+								</div>
+								<div class="form-group col-md-6">
+									<label for="onibus">Ônibus</label>
+									<select class="form-control" name="onibus" id="onibus">
+										<option value="SIM">SIM</option>
+										<option value="NAO">NÃO</option>
+									</select>
+								</div>
+							</div>
+							<div class="my-3">
+								<div class="loading">Loading</div>
+								<div class="error-message"></div>
+								<div class="sent-message">Your message has been sent. Thank you!</div>
+							</div>
+							<div class="text-center"><button type="submit">Finalizar</button></div>
+						</form>
+					</div>
+
+				</div>
+
+			</div>
+		</section><!-- End Contact Section -->
+
+	</main><!-- End #main -->
+
+	<!-- ======= Footer ======= -->
+	<footer id="footer">
+
+		<div class="container footer-bottom clearfix">
+			<div class="copyright">
+				&copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
+			</div>
+			<div class="credits">
+				<!-- All the links in the footer should remain intact. -->
+				<!-- You can delete the links only if you purchased the pro version. -->
+				<!-- Licensing information: https://bootstrapmade.com/license/ -->
+				<!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+				Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+			</div>
+		</div>
+	</footer><!-- End Footer -->
+
+	<div id="preloader"></div>
+	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+	<!-- Vendor JS Files -->
+	<script src="/site/assets/vendor/aos/aos.js"></script>
+	<script src="/site/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="/site/assets/vendor/glightbox/js/glightbox.min.js"></script>
+	<script src="/site/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+	<script src="/site/assets/vendor/swiper/swiper-bundle.min.js"></script>
+	<script src="/site/assets/vendor/waypoints/noframework.waypoints.js"></script>
+	<script src="/site/assets/vendor/php-email-form/validate.js"></script>
+
+	<!-- Template Main JS File -->
+	<script src="/site/assets/js/main.js"></script>
+
+</body>
+
+</html>
