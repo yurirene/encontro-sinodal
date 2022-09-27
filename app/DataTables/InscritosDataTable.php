de7a37444cdee5dc7277ddf845bbd5948d13514d
Yuri Ferreira <yuri.ferreira@flexpeak.com.br>
2022-09-27 12:37:35 -0400

    adicionando botao de exportação

--- app/DataTables/InscritosDataTable.php
+++ app/DataTables/InscritosDataTable.php
@@ -80,7 +80,7 @@ class InscritosDataTable extends DataTable
                         'language' => [
                             "url" => "//cdn.datatables.net/plug-ins/1.10.24/i18n/Portuguese-Brasil.json"
                         ],
-                        'buttons' => []
+                        'buttons' => ['excel', 'print']
                     ]);
     }
 
