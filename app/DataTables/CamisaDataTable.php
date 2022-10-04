79fdc6d4b0e310cd816710858eb29910139a1d2c
Yuri Ferreira <yuri.ferreira@flexpeak.com.br>
2022-10-04 08:46:34 -0400

    add excel camisa

--- app/DataTables/CamisaDataTable.php
+++ app/DataTables/CamisaDataTable.php
@@ -65,7 +65,7 @@ class CamisaDataTable extends DataTable
                         'language' => [
                             "url" => "//cdn.datatables.net/plug-ins/1.10.24/i18n/Portuguese-Brasil.json"
                         ],
-                        'buttons' => []
+                        'buttons' => ['excel']
                     ]);
     }
 
